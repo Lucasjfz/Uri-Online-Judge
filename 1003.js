94beb82d04d2ed10de66082a4d518e1bb8603827
Raimundo Geanderson Amador dos Santos <58758@POSITIVO.CORP>
2019-02-10 17:07:20 -0200

    start 1003

--- 1003.js
+++
@@ -1,8 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8')
-var lines = input.split('\n')
-
-var pi = 3.14159
-var r = parseFloat(lines.shift())
-r = r.toFixed(2)
-
-console.log('A=' + (pi*(Math.pow(r,2))).toFixed(4))
